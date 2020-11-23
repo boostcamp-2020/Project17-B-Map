204fa52e07a55f9bfea0635d461764b9f98d6f42
SeokHoKim <seokho0330@gmail.com>
2020-11-23 18:27:34 +0900

    feat: MarkerImageView 적용

--- BoostClusteringMaB/BoostClusteringMaB/ViewController.swift
+++ BoostClusteringMaB/BoostClusteringMaB/ViewController.swift
@@ -9,20 +9,8 @@ import UIKit
 import NMapsMap
 
 class ViewController: UIViewController {
-    let markerView: UIView = {
-        let view = UIView(frame: .init(x: 0, y: 0, width: 30, height: 30))
-        view.backgroundColor = .systemPink
-        return view
-    }()
-
-    let label: UILabel = {
-        let label = UILabel(frame: .init(x: 0, y: 0, width: 10, height: 10))
-        label.text = String(Int.random(in: 5000...50000))
-        label.backgroundColor = .yellow
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
+    let markerImageView = MarkerImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -36,27 +24,16 @@ class ViewController: UIViewController {
         let mapView = NMFMapView(frame: view.frame)
         view.addSubview(mapView)
 
-        markerView.addSubview(label)
-
-        NSLayoutConstraint.activate([
-            label.centerYAnchor.constraint(equalTo: markerView.centerYAnchor),
-            label.centerXAnchor.constraint(equalTo: markerView.centerXAnchor),
-            label.trailingAnchor.constraint(equalTo: markerView.trailingAnchor),
-            label.leadingAnchor.constraint(equalTo: markerView.leadingAnchor)
-        ])
-
-        markerView.layoutIfNeeded()
-
         let marker = NMFMarker(position: .init(lat: 37.3591784, lng: 127.1026379))
-        marker.setMarker(markerView)
+        marker.setMarker(markerImageView, count: 1)
         marker.mapView = mapView
 
         let marker2 = NMFMarker(position: .init(lat: 37.3561884, lng: 127.1026479))
-        marker2.setMarker(markerView)
+        marker2.setMarker(markerImageView, count: 2)
         marker2.mapView = mapView
 
         let marker3 = NMFMarker(position: .init(lat: 37.3501984, lng: 127.1026579))
-        marker3.setMarker(markerView)
+        marker3.setMarker(markerImageView, count: 3)
         marker3.mapView = mapView
 
         let lat = NMGLatLng(lat: 130, lng: 30)
@@ -68,25 +45,7 @@ class ViewController: UIViewController {
 }
 
 extension NMFMarker {
-    func setMarker(_ view: UIView) {
+    func setMarker(_ view: MarkerImageView, count: Int) {
         self.iconImage = NMFOverlayImage(image: view.snapshot())
-    }
-}
-
-extension UIView {
-    /// View를 UIImage로 생성
-    ///
-    /// 지정한 view를 이미지로 만들어줌
-    /// ```
-    /// let uiImage: UIImage = view.snapshot()
-    /// ```
-    /// - Returns: UIImage()
-    func snapshot(_ view: UIView...) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, UIScreen.main.scale)
-        guard let currentContext = UIGraphicsGetCurrentContext() else { return UIImage() }
-        self.layer.render(in: currentContext)
-        guard let img = UIGraphicsGetImageFromCurrentImageContext() else { return UIImage() }
-        UIGraphicsEndImageContext()
-        return img
     }
 }
