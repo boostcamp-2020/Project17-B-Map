b4de6979bb8c25d1a753f756a2c9ca6126dd0f7e
kati-kms <qkrguswndk@naver.com>
2020-11-23 15:02:19 +0900

    feat : LinkedList 자료구조 추가
    데이터 삭제시 O(1)로 시간을 줄이기 위함

---
+++ BoostClusteringMaB/BoostClusteringMaB/KMeans/LinkedList.swift
@@ -0,0 +1,119 @@
+//
+//  linked_list.swift
+//  BoostClusteringMaB
+//
+//  Created by 강민석 on 2020/07/29.
+//  Copyright © 2020 강민석. All rights reserved.
+//
+
+import Foundation
+
+class LinkedList<T: Equatable> {
+	var size: Int
+	var head: Node<T>?
+	var tail: Node<T>?
+	var now: Node<T>?
+	
+	init () {
+		self.size = 0
+		self.head = nil
+		self.tail = nil
+		self.now = nil
+	}
+	
+	func add(_ input: T) {
+		let new: Node<T> = Node(value: input)
+		new.next = nil
+		new.prev = self.tail
+		new.prev?.next = new
+		self.tail = new
+		if size == 0 {
+			head = new
+		}
+		self.size += 1
+	}
+	
+	func remove() -> T? {
+		//now node를 지운다
+		if now == head {
+			return linkedPopFront()
+		} else if now == tail {
+			return linekdPopBack()
+		} else {
+			let value = now?.value
+			let before = now?.prev
+			let after = now?.next
+			
+			before?.next = after
+			after?.prev = before
+			now = now?.prev
+			self.size -= 1
+			return value
+		}
+	}
+	
+	private func linkedPopFront() -> T? {
+		guard size == 0 else { return nil }
+		self.size -= 1
+		
+		let value = self.head?.value
+		self.head = self.head?.next
+		
+		if size == 0 {
+			tail = nil
+		} else {
+			self.head?.prev = nil
+		}
+		now = head
+		return value
+	}
+	
+	private func linekdPopBack() -> T? {
+		guard size == 0 else { return nil }
+		self.size -= 1
+		
+		let value = self.head?.value
+		self.tail = self.tail?.prev
+		
+		if size == 0 {
+			head = nil
+		} else {
+			self.tail?.next = nil
+		}
+		now = now?.prev
+		return value
+	}
+	
+	func setNowToHead() {
+		now = head
+	}
+	
+	func moveNowToNext() {
+		now = now?.next
+	}
+	
+	func allValues() -> [T] {
+		var values: [T?] = []
+		setNowToHead()
+		while now != nil {
+			values.append(now?.value)
+		}
+		return values.compactMap { $0 }
+	}
+}
+
+class Node<T: Equatable>: Equatable {
+	static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
+		return lhs.value == rhs.value
+	}
+	
+	var value: T
+	var next: Node<T>?
+	var prev: Node<T>?
+	
+	init(value: T) {
+		self.value = value
+		self.next = nil
+		self.prev = nil
+	}
+}
