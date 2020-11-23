d2dfb87f5ab76e4b219dd1ef5812e594180adefd
KYHyeon <run190822@gmail.com>
2020-11-23 16:08:06 +0900

    feat: 코어데이터 context 관리 객체 분리

--- BoostClusteringMaB/BoostClusteringMaB/SceneDelegate.swift
+++ BoostClusteringMaB/BoostClusteringMaB/SceneDelegate.swift
@@ -13,4 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        
+        // Save changes in the application's managed object context when the application transitions to the background.
+        CoreDataContainer.shared.saveContext()
+    }
 }
