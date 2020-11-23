33307339c85acac55e428d8ff601ec68df26a45d
KYHyeon <run190822@gmail.com>
2020-11-23 18:57:24 +0900

    test : POI 10000개 추가 성능 테스트 추가

--- BoostClusteringMaB/BoostClusteringMaBTests/CoreDataTests.swift
+++ BoostClusteringMaB/BoostClusteringMaBTests/CoreDataTests.swift
@@ -6,15 +6,16 @@
 //
 
 import XCTest
+import CoreData
 @testable import BoostClusteringMaB
 
 class CoreDataTests: XCTestCase {
     let newPlace = Place(category: "부스트캠프",
-                      id: "123321",
-                      imageUrl: nil,
-                      name: "Mab",
-                      x: "124.323412",
-                      y: "35.55532")
+                         id: "123321",
+                         imageUrl: nil,
+                         name: "Mab",
+                         x: "124.323412",
+                         y: "35.55532")
     
     func testAddPOI() throws {
         // Given
@@ -25,15 +26,15 @@ class CoreDataTests: XCTestCase {
         
         // Then
         let poi = try layer.fetch().first(where: { poi -> Bool in
-            poi.id == "123321"
+            poi.id == newPlace.id
         })
         
-        XCTAssertEqual(poi?.id, "123321")
-        XCTAssertEqual(poi?.category, "부스트캠프")
-        XCTAssertEqual(poi?.imageURL, nil)
-        XCTAssertEqual(poi?.name, "Mab")
-        XCTAssertEqual(poi?.latitude, 35.55532)
-        XCTAssertEqual(poi?.longitude, 124.323412)
+        XCTAssertEqual(poi?.id, newPlace.id)
+        XCTAssertEqual(poi?.category, newPlace.category)
+        XCTAssertEqual(poi?.imageURL, newPlace.imageUrl)
+        XCTAssertEqual(poi?.name, newPlace.name)
+        XCTAssertEqual(poi?.latitude, Double(newPlace.y))
+        XCTAssertEqual(poi?.longitude, Double(newPlace.x))
     }
     
     func testFetchPOI() throws {
@@ -44,6 +45,37 @@ class CoreDataTests: XCTestCase {
         let pois = try layer.fetch()
         
         // Then
-        print(pois)
+        XCTAssertNotNil(pois)
+        print(pois.count)
+    }
+    
+    func testAdd10000POI() throws {
+        // Given
+        let expectTimer = XCTestExpectation(description: "testAdd10000POI")
+        let asyncTimeout: TimeInterval = 100
+        let numberOfRepeats = 10000
+        let layer = CoreDataLayer()
+        let beforeCount = try layer.fetch().count
+        let group = DispatchGroup()
+        
+        // When
+        for _ in 0..<numberOfRepeats {
+            group.enter()
+            try? layer.add(place: newPlace) {
+                try? layer.save()
+                group.leave()
+            }
+        }
+        
+        // Then
+        group.notify(queue: .main) {
+            let fetchLayer = CoreDataLayer()
+            let afterCount = try? fetchLayer.fetch().count
+            XCTAssertEqual(beforeCount + numberOfRepeats, afterCount)
+            CoreDataContainer.shared.saveContext()
+            expectTimer.fulfill()
+        }
+        
+        wait(for: [expectTimer], timeout: asyncTimeout)
     }
 }
