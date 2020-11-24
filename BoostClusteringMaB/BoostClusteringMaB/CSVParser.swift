8ea88855ed9619b83b0ffb5d3040c5db904f6e9d
park <wogus3602@naver.com>
2020-11-24 15:03:31 +0900

    feat : CSVParser Class 생성
    1. CSV파일을 배열로 만드는 코드 작성
    2. CoreData에 add하는 코드 작성

--- BoostClusteringMaB/BoostClusteringMaB/CSVParser.swift
+++ BoostClusteringMaB/BoostClusteringMaB/CSVParser.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+class CSVParser {
+    enum CSVParSerError: Error {
+        case empty
+    }
+
+    var pois = [Place]()
+
+    func convertCSVIntoArray(file name: String) {
+        guard let filepath = Bundle.main.path(forResource: name, ofType: "csv"),
+              let data = try? String(contentsOfFile: filepath) else {
+            return
+        }
+
+        var rows = data.components(separatedBy: "\n")
+
+        rows.removeFirst()
+
+        for row in rows {
+            let columns = row.components(separatedBy: ",")
+
+            if columns.count == 6 {
+                let id = columns[0]
+                let name = columns[1]
+                let category = columns[2]
+                let x = columns[3]
+                let y = columns[4]
+                let imageURL = columns[5]
+
+                let place = Place(id: id, name: name, x: x, y: y, imageURL: imageURL, category: category)
+                pois.append(place)
+            }
+        }
+    }
+
+    func add(to coreDataManager: CoreDataManager) throws {
+        guard !pois.isEmpty else {
+            throw CSVParSerError.empty
+        }
+        do {
+        try pois.forEach({ place in
+            try coreDataManager.add(place: place, completion: nil)
+        })
+        } catch let error {
+            print(error)
+        }
+
+        try coreDataManager.save()
+    }
+}
