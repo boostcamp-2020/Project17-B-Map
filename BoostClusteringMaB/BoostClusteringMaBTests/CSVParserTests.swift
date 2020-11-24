e1657fce41f10da086bb6160b15658d26afc30d7
park <wogus3602@naver.com>
2020-11-24 16:24:44 +0900

    test : CSVParser throw test 추가

--- BoostClusteringMaB/BoostClusteringMaBTests/CSVParserTests.swift
+++ BoostClusteringMaB/BoostClusteringMaBTests/CSVParserTests.swift
@@ -8,32 +8,81 @@
 import XCTest
 @testable import BoostClusteringMaB
 
+class CoreDataLayerMock: CoreDataManager {
+    func add(place: Place, completion handler: (() -> Void)?) throws {
+
+    }
+
+    func fetch() throws -> [POI] {
+        []
+    }
+
+    func fetch(by classification: String) throws -> [POI] {
+        []
+    }
+
+    func remove(at: Int) throws {
+
+    }
+
+    func removeAll() throws {
+
+    }
+
+    func save() throws {
+
+    }
+}
+
 class CSVParserTests: XCTestCase {
 
-    func test_CSVParser_convertCSVIntoArray() throws {
+    func test_convertCSVIntoArray() throws {
         // Given
         let csvParser = CSVParser()
 
         // When
-        csvParser.convertCSVIntoArray(file: "poi")
+        try csvParser.convertCSVIntoArray(file: "poi")
 
         // Then
         XCTAssertNotNil(csvParser.pois)
         XCTAssertTrue(!csvParser.pois.isEmpty)
     }
 
-    func test_CSVParser_add() throws {
+    func test_add() throws {
         // Given
         let csvParser = CSVParser()
         let coreDataManager: CoreDataManager = CoreDataLayer()
         let beforeCount = try coreDataManager.fetch().count
 
         // When
-        csvParser.convertCSVIntoArray(file: "poi")
+        try csvParser.convertCSVIntoArray(file: "poi")
         try csvParser.add(to: coreDataManager)
 
         // Then
         let afterCount = try coreDataManager.fetch().count
         XCTAssertNotEqual(beforeCount, afterCount)
+    }
+
+    func test_add_poi빈배열() {
+        // Given
+        let csvParser = CSVParser()
+        let coreDataManagerMock: CoreDataManager = CoreDataLayerMock()
+
+        // Then
+        XCTAssertThrowsError(
+            // When
+            try csvParser.add(to: coreDataManagerMock)
+        )
+    }
+
+    func test_convertCSVIntoArray_존재하지않는파일입력() {
+        // Given
+        let csvParser = CSVParser()
+
+        // Then
+        XCTAssertThrowsError(
+            // When
+            try csvParser.convertCSVIntoArray(file: "존재하지않는파일")
+        )
     }
 }
