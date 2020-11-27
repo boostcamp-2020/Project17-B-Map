7c1f74358965d73d3428488bb7bd8af3ba190792
SeokHoKim <seokho0330@gmail.com>
2020-11-27 13:56:08 +0900

    refactor: 코드 정리
    - setMapView(markers:, mapView:) 추가
    - 카메라 시작점 lazy var 생성

--- BoostClusteringMaB/BoostClusteringMaB/ViewController.swift
+++ BoostClusteringMaB/BoostClusteringMaB/ViewController.swift
@@ -10,68 +10,71 @@ import NMapsMap
 
 class ViewController: UIViewController {
     lazy var naverMapView = NMFNaverMapView(frame: view.frame)
+    lazy var markerImageView = MarkerImageView(radius: 30)
     lazy var markerAnimationController = MarkerAnimateController(view: view, projection: mapView.projection)
+    lazy var startPoint = NMGLatLng(lat: 37.50378338836959, lng: 127.05559154398587) // 강남
+    
+    let coreDataLayer: CoreDataManager = CoreDataLayer()
+    
     var mapView: NMFMapView { naverMapView.mapView }
-    let markerImageView = MarkerImageView(radius: 30)
+    var projection: NMFProjection { naverMapView.mapView.projection }
     var markers = [NMFMarker]()
     var poiData: [POI]?
-
-    let coreDataLayer: CoreDataManager = CoreDataLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        try? coreDataLayer.removeAll()
-//        jsonToData(name: "gangnam_8000")
-//        jsonToData(name: "restaurant")
+        // try? coreDataLayer.removeAll()
+        // jsonToData(name: "gangnam_8000")
+        // jsonToData(name: "restaurant")
         configureMapView()
     }
     
     private func configureMapView() {
-        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: 37.50378338836959, lng: 127.05559154398587)) // 강남
         naverMapView.showZoomControls = true
         mapView.touchDelegate = self
         mapView.addCameraDelegate(delegate: self)
-        mapView.moveCamera(cameraUpdate)
+        mapView.moveCamera(.init(scrollTo: startPoint))
         view.addSubview(naverMapView)
     }
     
     private func jsonToData(name: String) {
-        if let path = Bundle.main.url(forResource: name, withExtension: "json") {
-            guard let data = try? Data(contentsOf: path) else { return  }
-            let jsonResult = try? JSONDecoder().decode(Places.self, from: data)
-            jsonResult?.places.forEach({
-                try? coreDataLayer.add(place: $0) {
-                    try? self.coreDataLayer.save()
-                }
-            })
+        guard let path = Bundle.main.url(forResource: name, withExtension: "json"),
+              let data = try? Data(contentsOf: path),
+              let jsonResult = try? JSONDecoder().decode(Places.self, from: data)
+        else { return }
+        
+        jsonResult.places.forEach {
+            try? coreDataLayer.add(place: $0) {
+                try? self.coreDataLayer.save()
+            }
         }
     }
-
+    
     func findOptimalClustering(completion: @escaping ([LatLng], [Int]) -> Void) {
         let boundsLatLngs = mapView.coveringBounds.boundsLatLngs
         let southWest = LatLng(boundsLatLngs[0])
         let northEast = LatLng(boundsLatLngs[1])
-
+        
         guard let points = try? coreDataLayer.fetch(southWest: southWest,
                                                     northEast: northEast, sorted: true).map({poi in
                                                         LatLng(lat: poi.latitude, lng: poi.longitude)
                                                     }) else { return }
-
+        
         guard !points.isEmpty else { return }
-
+        
         let kRange = (2...10)
-
+        
         var minValue = Double.greatestFiniteMagnitude
         var minKMeans: KMeans?
-
+        
         let group = DispatchGroup.init()
         let serialQueue = DispatchQueue.init(label: "serial")
-
+        
         kRange.forEach { k in
             DispatchQueue.global(qos: .userInteractive).async(group: group) {
                 let kMeans = KMeans(k: k, points: points)
                 kMeans.run()
-
+                
                 let DBI = kMeans.daviesBouldinIndex()
                 serialQueue.async(group: group) {
                     if DBI <= minValue {
@@ -111,28 +114,24 @@ class ViewController: UIViewController {
     }
     
     func convertLatLngToPoint(latLng: LatLng) -> CGPoint {
-        let projection = mapView.projection
-        let point = projection.point(from: NMGLatLng(lat: latLng.lat, lng: latLng.lng))
-        return point
+        return projection.point(from: NMGLatLng(lat: latLng.lat, lng: latLng.lng))
     }
 }
 
 extension ViewController: NMFMapViewCameraDelegate {
-    enum ClusteringAnimationType {
-        case merge, divide
+    private func createMarker(latLng: LatLng) -> NMFMarker {
+        return NMFMarker(position: NMGLatLng(lat: latLng.lat, lng: latLng.lng))
     }
     
-    func createMarker(latLng: LatLng) -> NMFMarker {
-        let marker = NMFMarker(position: NMGLatLng(lat: latLng.lat, lng: latLng.lng))
-        return marker
+    private func setMapView(makers: [NMFMarker], mapView: NMFMapView?) {
+        return markers.forEach { $0.mapView = mapView }
     }
     
     private func createMarkers(latLngs: [LatLng], pointSizes: [Int]) -> [NMFMarker] {
-        return zip(latLngs, pointSizes).map {
-            let marker = self.createMarker(latLng: $0)
-            if $1 != 1 {
-                marker.setImageView(self.markerImageView, count: $1)
-            }
+        return zip(latLngs, pointSizes).map { latLng, pointSize in
+            let marker = self.createMarker(latLng: latLng)
+            guard pointSize != 1 else { return marker }
+            marker.setImageView(self.markerImageView, count: pointSize)
             return marker
         }
     }
@@ -140,38 +139,32 @@ extension ViewController: NMFMapViewCameraDelegate {
     func mapViewCameraIdle(_ mapView: NMFMapView) {
         findOptimalClustering(completion: { [weak self] latLngs, pointSizes in
             guard let self = self else { return }
-
+            
             let newMarkers = self.createMarkers(latLngs: latLngs, pointSizes: pointSizes)
             
             guard self.markers.count != 0 else {
-                newMarkers.forEach { $0.mapView = self.mapView }
+                self.setMapView(makers: newMarkers, mapView: self.mapView)
                 self.markers = newMarkers
                 return
             }
             
-            self.markers.forEach({
-                $0.mapView = nil
-            })
+            self.setMapView(makers: self.markers, mapView: nil)
             
             self.markerAnimationController.clusteringAnimation(
                 old: self.markers.map { $0.position },
                 new: newMarkers.map { $0.position },
                 isMerge: self.markers.count > newMarkers.count) {
-                // after animation
                 self.markers = newMarkers
-                self.markers.forEach({
-                    $0.mapView = self.mapView
-                })
+                self.setMapView(makers: self.markers, mapView: self.mapView)
             }
         })
     }
-    
 }
 
 extension ViewController: NMFMapViewTouchDelegate {
     func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
         // MARK: - 화면 터치시 마커 찍기
-//        let marker = NMFMarker(position: latlng)
-//        marker.mapView = mapView
+        //        let marker = NMFMarker(position: latlng)
+        //        marker.mapView = mapView
     }
 }
