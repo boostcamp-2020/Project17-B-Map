1eb5fd4403eaa467b72a2c5a48bbf8b4cbba9e9d
park <wogus3602@naver.com>
2020-11-28 14:01:06 +0900

    style : 공백제거

--- BoostClusteringMaB/BoostClusteringMaBTests/CSVParserTests.swift
+++ BoostClusteringMaB/BoostClusteringMaBTests/CSVParserTests.swift
@@ -8,40 +8,6 @@
 import XCTest
 @testable import BoostClusteringMaB
 
-class CoreDataLayerMock: CoreDataManager {
-    func remove(poi: POI) throws {
-        
-    }
-    
-    func add(place: Place, completion handler: (() -> Void)?) throws {
-        
-    }
-
-    func add(places: [Place], completion handler: (() -> Void)?) throws {
-        
-    }
-
-    func fetch(sorted: Bool) throws -> [POI] {
-        []
-    }
-    
-    func fetch(by classification: String, sorted: Bool) throws -> [POI] {
-        []
-    }
-    
-    func fetch(southWest: LatLng, northEast: LatLng, sorted: Bool) throws -> [POI] {
-        return []
-    }
-    
-    func removeAll() throws {
-        
-    }
-    
-    func save() throws {
-        
-    }
-}
-
 class CSVParserTests: XCTestCase {
     
     func test_convertCSVIntoArray() throws {
