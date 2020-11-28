9c07e62c1b1605ed0ddc8347388601c651cce9b8
park <wogus3602@naver.com>
2020-11-28 15:48:39 +0900

    test : test_combineClusters, test_convertLatLngToPoint 작성

--- BoostClusteringMaB/BoostClusteringMaBTests/KMeans/ClusteringTests.swift
+++ BoostClusteringMaB/BoostClusteringMaBTests/KMeans/ClusteringTests.swift
@@ -19,6 +19,18 @@ class MapViewMock: NMFMapViewProtocol {
     }
 }
 
+class NMFProjectionMock: NMFProjection {
+    override func point(from coord: NMGLatLng) -> CGPoint {
+        return CGPoint(x: coord.lat, y: coord.lng)
+    }
+}
+
+class ClusterMock: Cluster {
+    override func combine(other: Cluster) {
+        self.center += other.center
+    }
+}
+
 class ClusteringTests: XCTestCase {
     func test_init() {
         // Given
@@ -32,16 +44,55 @@ class ClusteringTests: XCTestCase {
         XCTAssertNotNil(clustering)
     }
 
-    func test_init() {
+    func test_convertLatLngToPoint() {
         // Given
-        let mapViewMock = MapViewMock(coveringBounds: NMGLatLngBounds(), projection: NMFProjection())
+        let mapViewMock = MapViewMock(coveringBounds: NMGLatLngBounds(), projection: NMFProjectionMock())
         let coreDataLayerMock = CoreDataLayerMock()
-
-        // When
         let clustering = Clustering(naverMapView: mapViewMock, coreDataLayer: coreDataLayerMock)
 
+        // When
+        let point = clustering.convertLatLngToPoint(latLng: .init(lat: 30, lng: 50))
+
         // Then
-        XCTAssertNotNil(clustering)
+        XCTAssertEqual(point.x, 30)
+        XCTAssertEqual(point.y, 50)
     }
 
+//    func test_convertLatLngToPoint_NMGWebMercatorCoord() {
+//        // Given
+//        let mapViewMock = MapViewMock(coveringBounds: NMGLatLngBounds(), projection: NMFProjectionMock())
+//        let coreDataLayerMock = CoreDataLayerMock()
+//        let clustering = Clustering(naverMapView: mapViewMock, coreDataLayer: coreDataLayerMock)
+//
+//        // When
+//        let point = clustering.convertLatLngToPoint(latLng1: .zero, latLng2: .zero)
+//
+//        // Then
+//        XCTAssertEqual(point.x, 30)
+//        XCTAssertEqual(point.y, 50)
+//    }
+
+    func test_combineClusters() {
+        // Given
+        let mapViewMock = MapViewMock(coveringBounds: NMGLatLngBounds(), projection: NMFProjectionMock())
+        let coreDataLayerMock = CoreDataLayerMock()
+        let clustering = Clustering(naverMapView: mapViewMock, coreDataLayer: coreDataLayerMock)
+
+        let cluster1: [ClusterMock] = [ClusterMock(center: LatLng(lat: 1.0, lng: 1.0)),
+                                       ClusterMock(center: LatLng(lat: 90.0, lng: 90.0)),
+                                       ClusterMock(center: LatLng(lat: 2.0, lng: 1.0)),
+                                       ClusterMock(center: LatLng(lat: 3.0, lng: 1.0)),
+                                       ClusterMock(center: LatLng(lat: 4.0, lng: 1.0))]
+
+        // When
+        let clusters = clustering.combineClusters(clusters: cluster1)
+
+        // Then
+        XCTAssertEqual(clusters.count, 2)
+        XCTAssertEqual(clusters.first?.center, LatLng(lat: 10.0, lng: 4.0))
+        XCTAssertEqual(clusters.last?.center, LatLng(lat: 90.0, lng: 90.0))
+    }
+
+    func test_findOptimalClustering() {
+    }
 }
