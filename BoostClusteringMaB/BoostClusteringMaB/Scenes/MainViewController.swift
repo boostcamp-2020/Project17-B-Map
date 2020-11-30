19e201776a0b20c751ef276625bd8cbf2d585f32
SeokHoKim <seokho0330@gmail.com>
2020-11-30 18:04:20 +0900

    refactor: polygon 관련 refactoring

--- BoostClusteringMaB/BoostClusteringMaB/Scenes/MainViewController.swift
+++ BoostClusteringMaB/BoostClusteringMaB/Scenes/MainViewController.swift
@@ -75,12 +75,25 @@ final class MainViewController: UIViewController, MainDisplayLogic {
 }
 
 extension MainViewController: NMFMapViewCameraDelegate {
+    private func setMapView(overlays: [NMFOverlay], mapView: NMFMapView?) {
+        return overlays.forEach { $0.mapView = mapView }
+    }
+    
     private func createMarker(latLng: LatLng) -> NMFMarker {
         return NMFMarker(position: NMGLatLng(lat: latLng.lat, lng: latLng.lng))
     }
     
-    private func setMapView(overlays: [NMFOverlay], mapView: NMFMapView?) {
-        return overlays.forEach { $0.mapView = mapView }
+    private func createPolygonOverlay(points: [NMGLatLng]) -> NMFPolygonOverlay? {
+        let polygon = NMGPolygon(ring: NMGLineString(points: points)) as NMGPolygon<AnyObject>
+        guard let polygonOverlay = NMFPolygonOverlay(polygon) else { return nil }
+        
+        polygonOverlay.fillColor = UIColor(red: CGFloat(.random(in: 0.0...1.0)),
+                                           green: CGFloat(.random(in: 0.0...1.0)),
+                                           blue: CGFloat(.random(in: 0.0...1.0)),
+                                           alpha: 31.0/255.0)
+        polygonOverlay.outlineWidth = 3
+        polygonOverlay.outlineColor = UIColor(red: 25.0/255.0, green: 192.0/255.0, blue: 46.0/255.0, alpha: 1)
+        return polygonOverlay
     }
     
     private func createMarkers(latLngs: [LatLng], pointSizes: [Int]) -> [NMFMarker] {
@@ -112,30 +125,16 @@ extension MainViewController: NMFMapViewCameraDelegate {
             })
     }
     
+    // MARK: - 영역표시
     private func changePolygonOverays(points convexHullPoints: [[LatLng]]) {
-        setMapView(overlays: polygonOverlays, mapView: nil)
-        polygonOverlays.removeAll()
+        polygonOverlays = convexHullPoints
+            .filter { $0.count > 3 }
+            .compactMap { latlngs in
+                let points = latlngs.map { NMGLatLng(lat: $0.lat, lng: $0.lng) }
+                return createPolygonOverlay(points: points)
+            }
         
-        // MARK: - 영역표시
-        for latlngs in convexHullPoints where latlngs.count > 3 {
-            let points = latlngs.map { NMGLatLng(lat: $0.lat, lng: $0.lng) }
-            
-            let polygon = NMGPolygon(ring: NMGLineString(points: points)) as NMGPolygon<AnyObject>
-            guard let polygonOverlay = NMFPolygonOverlay(polygon) else { continue }
-            
-            let randomNumber1 = CGFloat(Double.random(in: 0.0...1.0))
-            let randomNumber2 = CGFloat(Double.random(in: 0.0...1.0))
-            let randomNumber3 = CGFloat(Double.random(in: 0.0...1.0))
-            
-            polygonOverlay.fillColor = UIColor(red: randomNumber1,
-                                               green: randomNumber2,
-                                               blue: randomNumber3,
-                                               alpha: 31.0/255.0)
-            polygonOverlay.outlineWidth = 3
-            polygonOverlay.outlineColor = UIColor(red: 25.0/255.0, green: 192.0/255.0, blue: 46.0/255.0, alpha: 1)
-            polygonOverlay.mapView = self.naverMapView.mapView
-            self.polygonOverlays.append(polygonOverlay)
-        }
+        setMapView(overlays: polygonOverlays, mapView: mapView)
     }
     
     func mapViewCameraIdle(_ mapView: NMFMapView) {
@@ -148,6 +147,8 @@ extension MainViewController: NMFMapViewCameraDelegate {
                 self.configureFirstMarkers(newMarkers: newMarkers)
                 return
             }
+            
+            self.setMapView(overlays: self.polygonOverlays, mapView: nil)
             
             self.markerChangeAnimation(
                 newMarkers: newMarkers,
