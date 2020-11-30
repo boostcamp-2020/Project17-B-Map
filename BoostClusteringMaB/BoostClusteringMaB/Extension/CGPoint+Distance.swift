e74fe368de0b522d55277971b475d6ff7cd6f2fd
kati-kms <qkrguswndk@naver.com>
2020-11-30 18:53:25 +0900

    feat : 화면좌표를 NMFLatLng로 변경하는 함수

--- BoostClusteringMaB/BoostClusteringMaB/Extension/CGPoint+Distance.swift
+++ BoostClusteringMaB/BoostClusteringMaB/Extension/CGPoint+Distance.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import NMapsMap
 
 extension CGPoint {
     func distance(to point: CGPoint) -> Double {
         return Double(sqrt(pow((point.x - x), 2) + pow((point.y - y), 2)))
+    }
+    
+    func convert(mapView: NMFMapView) -> NMGLatLng {
+        let projection = mapView.projection
+        let coord = projection.latlng(from: self)
+        return coord
     }
 }
