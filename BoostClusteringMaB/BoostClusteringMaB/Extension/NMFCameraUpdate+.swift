3341345e4f4f0d4ed1a0db43eb24e3e53aae41d8
park <wogus3602@naver.com>
2020-12-11 00:36:53 +0900

    chore : NMFCameraUpdate init추가

--- BoostClusteringMaB/BoostClusteringMaB/Extension/NMFCameraUpdate+.swift
+++ BoostClusteringMaB/BoostClusteringMaB/Extension/NMFCameraUpdate+.swift
@@ -5,4 +5,24 @@
 //  Created by ParkJaeHyun on 2020/12/11.
 //
 
-import Foundation
+import NMapsMap
+
+extension NMFCameraUpdate {
+    convenience init(fit: NMGLatLngBounds,
+                     paddingInsets: UIEdgeInsets,
+                     cameraAnimation: NMFCameraUpdateAnimation,
+                     duration: Double) {
+        self.init(fit: fit, paddingInsets: paddingInsets)
+        animation = cameraAnimation
+        animationDuration = duration
+    }
+
+    convenience init(scrollTo: NMGLatLng,
+                     zoomTo: Double,
+                     cameraAnimation: NMFCameraUpdateAnimation,
+                     duration: Double) {
+        self.init(scrollTo: scrollTo, zoomTo: zoomTo)
+        animation = cameraAnimation
+        animationDuration = duration
+    }
+}
