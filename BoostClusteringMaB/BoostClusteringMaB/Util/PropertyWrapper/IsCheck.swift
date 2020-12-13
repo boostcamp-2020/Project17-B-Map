ac2c3f3dd5f0571a8d0b55988005f92ac6506898
park <wogus3602@naver.com>
2020-12-14 00:16:53 +0900

    feat : DrawerModels 생성

---
+++ BoostClusteringMaB/BoostClusteringMaB/Util/PropertyWrapper/IsCheck.swift
@@ -0,0 +1,8 @@
+//
+//  IsCheck.swift
+//  BoostClusteringMaB
+//
+//  Created by ParkJaeHyun on 2020/12/14.
+//
+
+import Foundation
---
+++ BoostClusteringMaB/BoostClusteringMaB/Util/PropertyWrapper/Value.swift
@@ -0,0 +1,8 @@
+//
+//  Value.swift
+//  BoostClusteringMaB
+//
+//  Created by ParkJaeHyun on 2020/12/14.
+//
+
+import Foundation
