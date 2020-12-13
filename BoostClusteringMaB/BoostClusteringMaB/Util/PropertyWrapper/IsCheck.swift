043263587e0d8d57278684947f47d1700ddc6a24
park <wogus3602@naver.com>
2020-12-14 00:18:19 +0900

    feat : propert wrapper 생성

--- BoostClusteringMaB/BoostClusteringMaB/Util/PropertyWrapper/IsCheck.swift
+++ BoostClusteringMaB/BoostClusteringMaB/Util/PropertyWrapper/IsCheck.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct IsCheck {
+    private let key: String
+    
+    var wrappedValue: Bool {
+        get { UserDefaults.standard.bool(forKey: key) }
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+    }
+    
+    init(key: String) {
+        self.key = key
+    }
+}
--- BoostClusteringMaB/BoostClusteringMaB/Util/PropertyWrapper/Value.swift
+++ BoostClusteringMaB/BoostClusteringMaB/Util/PropertyWrapper/Value.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct Value {
+    private let key: String
+    
+    var wrappedValue: Float {
+        get { UserDefaults.standard.float(forKey: key) }
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+    }
+    
+    init(key: String) {
+        self.key = key
+    }
+}
